9bd935fe1e58815cd9765c02f5572d592b268b53
sozoia <zfzg2232@gmail.com>
2024-10-12 01:34:09 +0300

    pascal tringle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    '''construct to the nth pascal tringle row'''
+    
+    if n < 0:
+        return []
+    
+    tringle = []
+
+    for row in range(n):
+        elements = []
+        elements.append(1)
+        if row == 0:
+            tringle.append(elements)
+            continue
+        elif (row > 0):
+            for j in range(1,row):
+                # note: when ever you retrive kind of a serial its better to
+                # start from 1 to the index
+                # get elements j and j-1 to avoid out of range 
+                elements.append(tringle[row-1][j-1] + tringle[row-1][j])
+        elements.append(1)
+        tringle.append(elements)
+    return tringle
