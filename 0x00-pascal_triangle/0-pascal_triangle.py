148aba5d3f96264bc19698e8cc2e115d1813fdfb
sozoia <zfzg2232@gmail.com>
2024-12-12 22:24:02 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,25 +5,19 @@ alx interview tasks
 '''
 
 def pascal_triangle(n):
-    '''construct to the nth row pascal tringle'''
+    '''construct to the nth pascal tringle row'''
     
     if n < 0:
         return []
-    
-    tringle = []
+    if  n == 0:
+        return [[1]]
 
-    for row in range(n):
-        elements = []
-        elements.append(1)
-        if row == 0:
-            tringle.append(elements)
-            continue
-        elif (row > 0):
-            for j in range(1,row):
-                # note: when ever you retrive kind of a serial its better to
-                # start from 1 to the index
-                # get elements j and j-1 to avoid out of range 
-                elements.append(tringle[row-1][j-1] + tringle[row-1][j])
-        elements.append(1)
-        tringle.append(elements)
+    tringle = [[1]]
+    
+    for i in range(1,n):
+        element = [1]
+        for j in range(1,i):
+            element.append(tringle[i-1][j-1] + tringle[i-1][j])
+        element.append(1)
+        tringle.append(element)
     return tringle
--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 
 '''
-in this module we will be solving intersting problem which is prime game problem
-
-the idea is that we will be given a number of roundes that two players will play and array contains numbers 
-
-for each round there will be a given n number from the array
-then starting from the player one that will choose prime numbers which is from 0 - n
-when the player choose the number the number and its multiplys will be marked which means they can't be choosen anymore
-the game goes on like this unitl the first player who can't find any prime number to choose will be the loser of the game
-
+    prime game problem
 '''
 
+
 import math
+
 
 def prime(n):
     '''
@@ -20,7 +14,6 @@ def prime(n):
     @n: limit
     Return: list of boolean of nummbers that are prime
     '''
-
     if n < 0:
         return []
     elif n <= 1:
@@ -28,21 +21,20 @@ def prime(n):
 
     nums = [True for _ in range(n+1)]
     num = 2
-
     nums[0] = False
     nums[1] = False
 
     while(num < math.sqrt(n)):
         if nums[num]:
-            '''the nummber is not marked so its prime then we update the array'''
-            for mul in range(num ** 2,n + 1, num):
+            # the nummber is not marked so its prime then we update the array
+            for mul in range(num ** 2, n + 1, num):
                 '''mark all mul of that numer as composite'''
                 nums[mul] = False
         num += 1
     return nums
 
 
-def req_sol(turn,primes):
+def req_sol(turn, primes):
     '''
     @primes: list of prime numbers
     '''
@@ -53,20 +45,21 @@ def req_sol(turn,primes):
             return False
         else:
             return True
-    
+
     winner = False
     for prime in primes:
         if turn == 1:
-            primes = list(filter(lambda x: (x != prime) and (x % prime != 0), primes))
-            winner  = req_sol(2,primes)
+            primes = list(filter(lambda x: (x != prime)), primes)
+            winner = req_sol(2, primes)
         elif turn == 2:
-            primes = list(filter(lambda x: (x != prime) and (x % prime != 0), primes))
-            winner = req_sol(1,primes)
-        
+            primes = list(filter(lambda x: (x != prime), primes))
+            winner = req_sol(1, primes)
+
         if winner:
-            return False # this mean the base case returned True (that the first player don't have choise)
+            return False
 
     return True
+
 
 def isWinner(x, nums):
     '''
@@ -75,28 +68,26 @@ def isWinner(x, nums):
     @nums: array contain n for each round
     Return: the winner
     '''
-    p1,p2 = 0,0
+    p1, p2 = 0, 0
 
     for round in range(x):
         prime_list = []
         # for each round
-        for ind,num in enumerate(prime(nums[round])):
+        for ind, num in enumerate(prime(nums[round])):
             if num:
                 prime_list.append(ind)
 
         # print(prime_list)
-        winner = req_sol(1,prime_list)
+        winner = req_sol(1, prime_list)
 
         if winner:
             # print(f'round {round} p1 won')
-            p1 +=1
+            p1 += 1
         else:
-            #print(f'round {round} p2 won')
-            p2 +=1
+            # print(f'round {round} p2 won')
+            p2 += 1
 
     if p2 > p1:
-        print('Winner: Ben')
+        return 'Ben'
     else:
-        print('Winner: Maria')
-
-# isWinner(3, [4,5,1])
+        return 'Maria'
